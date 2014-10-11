97ee381a54bf06061041b8fb92aaa39aad7cc202
Saleh Albatati <0xack13@gmail.com>
2014-10-12 00:10:23 +0300

    Basic drawing is working..

--- touchDrawing/touchDrawing/ViewController.m
+++ touchDrawing/touchDrawing/ViewController.m
@@ -42,9 +42,34 @@
     lastPoint = [touch locationInView:self.view];
     lastPoint.y -= 0;
     
-    [super touchesBegan:touches withEvent:event];
+    [super touchesBegan: touches withEvent: event];
 }
 
+- (void) touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    mouseSwiped = YES;
+    
+    UITouch *touch = [touches anyObject];
+    currentPoint = [touch locationInView:self.view];
+    
+    UIGraphicsBeginImageContext(CGSizeMake(320, 568));
+    [drawImage.image drawInRect:CGRectMake(0,0, 320, 568)];
+    CGContextSetLineCap(UIGraphicsGetCurrentContext(), kCGLineCapRound);
+    
+    CGContextSetLineWidth(UIGraphicsGetCurrentContext(), 5.0);
+    CGContextSetRGBStrokeColor(UIGraphicsGetCurrentContext(), 0, 1, 0, 1);
+    CGContextBeginPath(UIGraphicsGetCurrentContext());
+    CGContextMoveToPoint(UIGraphicsGetCurrentContext(), lastPoint.x, lastPoint.y);
+    CGContextAddLineToPoint(UIGraphicsGetCurrentContext(), currentPoint.x, currentPoint.y);
+    CGContextStrokePath(UIGraphicsGetCurrentContext());
+    
+    [drawImage setFrame:CGRectMake(0,0, 320, 568)];
+    drawImage.image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    lastPoint = currentPoint;
+    
+    [self.view addSubview:drawImage];
+}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
