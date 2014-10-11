39f90fa3059a42c5612ea3409da42ca9c92426bd
Saleh Albatati <0xack13@gmail.com>
2014-10-11 23:51:30 +0300

    Add touch event

--- touchDrawing/touchDrawing/ViewController.m
+++ touchDrawing/touchDrawing/ViewController.m
@@ -16,8 +16,33 @@
 
 - (void)viewDidLoad
 {
+    
+    //Initialize Drawing Code
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    drawImage.image = [defaults objectForKey:@"drawImageKey"];
+    drawImage = [[UIImageView alloc] initWithImage:nil];
+    drawImage.frame = self.view.frame;
+    [self.view addSubview:drawImage];
+    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    UITouch *touch = [[event allTouches] anyObject];
+    
+    if([touch tapCount] == 2) {
+        drawImage.image = nil;
+    }
+    
+    location = [touch locationInView:touch.view];
+    lastClick = [NSDate date];
+    
+    lastPoint = [touch locationInView:self.view];
+    lastPoint.y -= 0;
+    
+    [super touchesBegan:touches withEvent:event];
 }
 
 - (void)didReceiveMemoryWarning
