d4325c3f1c5f9f8b22c5c3dd35f93a54e0b05864
Saleh Albatati <0xack13@gmail.com>
2014-10-11 23:40:59 +0300

    Initial Commit

---
+++ touchDrawing/touchDrawing/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  touchDrawing
+//
+//  Created by 0xack13 on 10/11/14.
+//  Copyright (c) 2014 0xack13. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ touchDrawing/touchDrawingTests/touchDrawingTests.m
@@ -0,0 +1,34 @@
+//
+//  touchDrawingTests.m
+//  touchDrawingTests
+//
+//  Created by 0xack13 on 10/11/14.
+//  Copyright (c) 2014 0xack13. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface touchDrawingTests : XCTestCase
+
+@end
+
+@implementation touchDrawingTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
